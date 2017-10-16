86870810b79025f201b6528d36a096930f7a59f8
iwakawa <df533738@db3.so-net.ne.jp>
2017-10-16 17:37:58 +0900

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+csv_data = CSV.read('BTC.csv', headers: false)
+
+Gain.new(purchase: csv_data[0][0], gain_price: csv_data[0][1])
+
+
+Gain.new(purchase: csv_data[0], gain_price: csv_data[1])
+
+
+csv_data.each {|data| Gain.create(purchase: Time.strptime(csv_data[0], "%Y年%m月%d日"), gain_price: csv_data[1])}
